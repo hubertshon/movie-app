f927825fee0d9fc89697011faf6de6b487eb6316
hubert <huby40@gmail.com>
2020-05-27 20:40:14 -0400

    adds Ffaker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-  actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
-  actor.save
+require 'ffaker'
+  # actor = Actor.new({first_name: "Benedict", last_name: "Cumberbatch", known_for: "Sherlock" })
+  # actor.save
 
-  actor = Actor.new({first_name: "Amy", last_name: "Adams", known_for: "Arrival" })
+  # actor = Actor.new({first_name: "Amy", last_name: "Adams", known_for: "Arrival" })
+  # actor.save
+
+10.times do 
+  actor = Actor.new({first_name: FFaker::Name.first_name, last_name: FFaker::Name.last_name, known_for: Ffaker::title})
   actor.save
+end 
 
